2b89f14b5f711286a8c50ea2fbfb048739c14df6
prosavage <naman0311@gmail.com>
2019-09-25 05:36:04 -0500

    Remove debug code from Go Command.

--- src/main/kotlin/net/savagellc/savageskyblock/command/cmd/CmdGo.kt
+++ src/main/kotlin/net/savagellc/savageskyblock/command/cmd/CmdGo.kt
@@ -13,27 +13,15 @@ class CmdGo : SCommand() {
         aliases.add("go")
         this.optionalArgs.add("player")
 
-        // TODO: Remove later after testing :)
-//        this.optionalArgs.add("fill")
 
         commandRequirements = CommandRequirementsBuilder().asIslandMember(true).asPlayer(true).withPermission(Permission.GO).build()
     }
 
 
     override fun perform(info: CommandInfo) {
-
         val island = info.iPlayer!!.getIsland()!!
         info.message(Message.commandGoTeleporting)
         info.player!!.teleport(island.getIslandSpawn())
-//        if (CommandInfo.args.size > 0) {
-//            if (CommandInfo.args[0] == "fill") {
-//                island.fillIsland(Material.valueOf(CommandInfo.args[0]))
-//            } else if (CommandInfo.args[0] == "dummy") {
-//                val createIsland = createIsland(null, "island")
-//                createIsland.fillIsland(Material.BROWN_WOOL)
-//            }
-////        }
-
     }
 
 
