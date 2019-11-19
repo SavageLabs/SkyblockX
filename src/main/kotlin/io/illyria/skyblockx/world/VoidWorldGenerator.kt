30171e3f6e40a01fcd3d332c83906b13c66b4116
ProSavage <naman0311@gmail.com>
2019-11-18 23:18:29 -0600

    Set biome to plains by default.

--- src/main/kotlin/io/illyria/skyblockx/world/VoidWorldGenerator.kt
+++ src/main/kotlin/io/illyria/skyblockx/world/VoidWorldGenerator.kt
@@ -2,6 +2,7 @@ package io.illyria.skyblockx.world
 
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.block.Biome
 import org.bukkit.generator.BlockPopulator
 import org.bukkit.generator.ChunkGenerator
 import java.util.*
@@ -17,12 +18,19 @@ class VoidWorldGenerator : ChunkGenerator() {
     }
 
     fun generate(world: World, rand: Random, chunkx: Int, chunkz: Int): ByteArray {
-        return ByteArray(32768)
+        return ByteArray(world.getMaxHeight() / 16)
     }
 
     override fun generateChunkData(world: World, random: Random, x: Int, z: Int, biome: BiomeGrid): ChunkData {
-        return createChunkData(world)
+        val createChunkData = createChunkData(world)
+        for (cx in 0..15) {
+            for (cz in 0..15) {
+                biome.setBiome(cx, cz, Biome.PLAINS)
+            }
+        }
+        return createChunkData
     }
+
 
 
     override fun getFixedSpawnLocation(world: World, random: Random): Location {
