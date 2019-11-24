69657cb0fec5304c6f3a67c7db30d7240ab845d9
ProSavage <naman0311@gmail.com>
2019-11-23 21:12:31 -0600

    6 intial setup quests.

--- src/main/kotlin/io/illyria/skyblockx/persist/Quests.kt
+++ src/main/kotlin/io/illyria/skyblockx/persist/Quests.kt
@@ -39,7 +39,7 @@ object Quests {
                 ),
                 1
             ),
-            14,
+            0,
             QuestGoal.BREAK_BLOCKS,
             XMaterial.OAK_LOG.name,
             1,
@@ -60,16 +60,16 @@ object Quests {
         ),
         Quest(
             "Quest-2",
-            "Craft wooden planks",
+            "Craft a wooden pickaxe",
             SerializableItem(
-                XMaterial.OAK_PLANKS,
-                "&bCraft some wooden planks",
-                listOf("&7Put a log in a crafting table", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
+                XMaterial.WOODEN_PICKAXE,
+                "&bCraft a Wooden Pickaxe",
+                listOf("&7Put three planks and 2 sticks in a crafting table.", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
                 1
             ),
-            15,
+            1,
             QuestGoal.CRAFT,
-            XMaterial.OAK_PLANKS.name,
+            XMaterial.WOODEN_PICKAXE.name,
             1,
             true,
             QuestActions(
@@ -90,19 +90,19 @@ object Quests {
             "Mine-Cobblestone-1",
             SerializableItem(
                 XMaterial.LAVA_BUCKET,
-                "&8Create a Cobblestone generator and mine a block from it.",
+                "&8Create an Ore Generator and mine a cobblestone block from it.",
                 listOf("&7Mine &b1 &7cobblestone", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
                 1
             ),
-            0,
+            2,
             QuestGoal.BREAK_BLOCKS,
             XMaterial.COBBLESTONE.name,
-            10,
+            1,
             true,
             QuestActions(
                 listOf(
                     "message(&7You have started the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
-                    "actionbar(&7Your quest progress is {quest-amount-completed}/{quest-amount-till-complete})"
+                    "actionbar(&7Your quest progress is {quest-amount-till-complete})"
                 )
             ),
             QuestActions(
@@ -114,22 +114,76 @@ object Quests {
         ),
         Quest(
             "Quest-4",
-            "Mine 10 Cobblestone",
+            "Mine 3 Iron Ore",
             SerializableItem(
-                XMaterial.COBBLESTONE,
-                "&8Create a Cobblestone generator and mine a block from it.",
-                listOf("&7Mine &b1 &7cobblestone", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
+                XMaterial.IRON_ORE,
+                "&8Mine 3 Iron Ore from the Ore Generator.",
+                listOf("&7Mine &b3 Iron Ore.", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
                 1
             ),
-            0,
+            3,
             QuestGoal.BREAK_BLOCKS,
-            XMaterial.COBBLESTONE.name,
+            XMaterial.IRON_ORE.name,
             10,
             true,
             QuestActions(
                 listOf(
                     "message(&7You have started the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
-                    "actionbar(&7Your quest progress is {quest-amount-completed}/{quest-amount-till-complete})"
+                    "actionbar(&7Your quest progress is {quest-amount-till-complete})"
+                )
+            ),
+            QuestActions(
+                listOf(
+                    "message(&7You have finished the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
+                    "title(&7{quest-name}:::&b&lQuest Completed)"
+                )
+            )
+        ),
+        Quest(
+            "Quest-5",
+            "Craft an Furnace",
+            SerializableItem(
+                XMaterial.FURNACE,
+                "&8Craft a Furnace",
+                listOf("&7Place 8 cobblestone blocks in the crafting table.", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
+                1
+            ),
+            4,
+            QuestGoal.CRAFT,
+            XMaterial.FURNACE.name,
+            10,
+            true,
+            QuestActions(
+                listOf(
+                    "message(&7You have started the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
+                    "actionbar(&7Your quest progress is {quest-amount-till-complete})"
+                )
+            ),
+            QuestActions(
+                listOf(
+                    "message(&7You have finished the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
+                    "title(&7{quest-name}:::&b&lQuest Completed)"
+                )
+            )
+        ),
+        Quest(
+            "Quest-6",
+            "Craft an &bIron Pickaxe",
+            SerializableItem(
+                XMaterial.IRON_PICKAXE,
+                "&8Craft an Iron Pickaxe",
+                listOf("&7Place three Iron Ingots and 2 Sticks into a crafting table.", "&7Completion: &b{currentAmount}&7/&b{finalAmount}"),
+                1
+            ),
+            5,
+            QuestGoal.CRAFT,
+            XMaterial.IRON_PICKAXE.name,
+            10,
+            true,
+            QuestActions(
+                listOf(
+                    "message(&7You have started the {quest-name}:::&7Your current progress is {quest-amount-till-complete})",
+                    "actionbar(&7Your quest progress is {quest-amount-till-complete})"
                 )
             ),
             QuestActions(
