64ff79cf666f2871088d7e006a966f1e64f923c6
prosavage <naman0311@gmail.com>
2019-12-02 15:49:42 -0600

    Delete command

---
+++ src/main/kotlin/io/illyria/skyblockx/command/skyblock/cmd/CmdSbDelete.kt
@@ -0,0 +1,39 @@
+package io.illyria.skyblockx.command.skyblock.cmd
+
+import io.illyria.skyblockx.Globals
+import io.illyria.skyblockx.command.CommandInfo
+import io.illyria.skyblockx.command.CommandRequirementsBuilder
+import io.illyria.skyblockx.command.SCommand
+import io.illyria.skyblockx.core.getIPlayer
+import io.illyria.skyblockx.core.getIPlayerByName
+import io.illyria.skyblockx.persist.Message
+
+
+class CmdSbDelete : SCommand() {
+
+
+    init {
+        aliases.add("delete")
+        aliases.add("remove")
+
+        requiredArgs.add(Argument("owner-tag", 0, PlayerArgument()))
+
+        commandRequirements = CommandRequirementsBuilder().build()
+    }
+
+    override fun perform(info: CommandInfo) {
+        val iPlayerByName = getIPlayerByName(info.args[0])
+        if (iPlayerByName?.getIsland() == null) {
+            info.message(Message.commandSkyblockRemoveNotAnIslandOwner)
+            return
+        }
+        iPlayerByName.getIsland()?.delete()
+        info.message(Message.commandSkyblockRemoveSuccess)
+
+    }
+
+    override fun getHelpInfo(): String {
+        return Message.commandSkyblockRemoveHelp
+    }
+
+}
