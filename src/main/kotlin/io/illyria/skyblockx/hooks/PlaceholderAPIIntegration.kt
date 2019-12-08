01842cbeaa47283e1bf0adf3789fdb38aba2e5bd
ProSavage <naman0311@gmail.com>
2019-12-08 08:21:35 -0600

    :gear: Worth Placeholders

--- src/main/kotlin/io/illyria/skyblockx/hooks/PlaceholderAPIIntegration.kt
+++ src/main/kotlin/io/illyria/skyblockx/hooks/PlaceholderAPIIntegration.kt
@@ -4,10 +4,14 @@ import io.illyria.skyblockx.Globals
 import io.illyria.skyblockx.core.getIPlayer
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
 import org.bukkit.entity.Player
+import java.text.DecimalFormat
 import java.util.stream.Collectors.joining
 
 
 class PlacholderAPI : PlaceholderExpansion() {
+
+    val decimalFormat = DecimalFormat()
+
     override fun getIdentifier(): String {
         return "skyblockx"
     }
@@ -30,7 +34,8 @@ class PlacholderAPI : PlaceholderExpansion() {
             "island_owner" -> iPlayer.getIsland()?.ownerTag ?: "N/A"
             "island_member_amount", "island_member_count" -> iPlayer.getIsland()?.getAllMembers()?.size.toString() ?: "0"
             "island_member_list" -> iPlayer.getIsland()?.getAllMembers()?.stream()!!.collect(joining(", ")) ?: "N/A"
-
+            "island_worth" -> Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth.toString() ?: "0"
+            "island_worth_formatted" -> decimalFormat.format(Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth ?: 0)
             else -> null
         }
     }
