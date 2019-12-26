c00d1a91bd10d2b069a102d9fac04b25a7943faa
prosavage <naman0311@gmail.com>
2019-12-25 20:27:11 -0600

    :gear: Added events for API

---
+++ src/main/kotlin/io/illyria/skyblockx/event/IslandPostLevelCalcEvent.kt
@@ -0,0 +1,21 @@
+package io.illyria.skyblockx.event
+
+import io.illyria.skyblockx.core.Island
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class IslandPostLevelCalcEvent(val island: Island, val levelAfterCalc: Double?) : Event(true) {
+
+    private val HANDLERS = HandlerList()
+    private var isCancelled = false
+
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
+
+    fun getHandlerList(): HandlerList? {
+        return HANDLERS
+    }
+
+}
