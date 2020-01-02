feabc157437c750e3db3d346c8488db486944b0b
ProSavage <naman0311@gmail.com>
2020-01-02 10:57:49 -0600

    :gear: Setter for level in IslandPostLevelCalcEvent.kt

--- src/main/kotlin/io/illyria/skyblockx/event/IslandPostLevelCalcEvent.kt
+++ src/main/kotlin/io/illyria/skyblockx/event/IslandPostLevelCalcEvent.kt
@@ -7,6 +7,10 @@ import org.bukkit.event.HandlerList
 
 class IslandPostLevelCalcEvent(val island: Island, var levelAfterCalc: Double?) : Event(true) {
 
+    fun setLevelAfterCalc(levelAfterCalc: Double) {
+        this.levelAfterCalc = levelAfterCalc
+    }
+
     private val HANDLERS = HandlerList()
     private var isCancelled = false
 
