961b5a5ca7beb45d81647788388586c442cbe9c5
prosavage <naman0311@gmail.com>
2020-04-05 03:11:10 -0500

    island_member_online_amount

--- src/main/kotlin/net/savagelabs/skyblockx/hooks/PlaceholderAPIIntegration.kt
+++ src/main/kotlin/net/savagelabs/skyblockx/hooks/PlaceholderAPIIntegration.kt
@@ -33,15 +33,21 @@ class PlacholderAPIIntegration : PlaceholderExpansion() {
         val iPlayer = getIPlayer(player)
         return when (s) {
             "island_owner" -> iPlayer.getIsland()?.ownerTag ?: "N/A"
-            "island_member_amount", "island_member_count" -> iPlayer.getIsland()?.getIslandMembers()?.size?.toString()
-                ?: "0"
-            "island_member_online_amount", "island_member_online_count" -> iPlayer.getIsland()?.getIslandMembers()
-                ?.filter { member -> member.isOnline() }?.size?.toString() ?: "0"
+            "island_member_amount", "island_member_count" -> (iPlayer.getIsland()?.getIslandMembers()?.size?.plus(1)
+                ?: 0).toString()
+            "island_member_online_amount", "island_member_online_count" -> (iPlayer.getIsland()?.getIslandMembers()
+                ?.filter { member -> member.isOnline() }?.size?.plus(
+                if (iPlayer.getIsland()?.getOwnerIPlayer()?.isOnline() == true) 1 else 0
+            ) ?: 0).toString()
             "island_member_list" -> iPlayer.getIsland()?.getIslandMembers()?.stream()!!.map { member -> member.name }
                 .collect(joining(", ")) ?: "N/A"
             "island_worth" -> Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth?.toString() ?: "0"
-            "island_worth_formatted" -> decimalFormat.format(Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth ?: 0)
-            "island_worth_rounded" -> floor(Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth ?: 0.0).toString()
+            "island_worth_formatted" -> decimalFormat.format(
+                Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth ?: 0
+            )
+            "island_worth_rounded" -> floor(
+                Globals.islandValues?.map?.get(iPlayer.getIsland()?.islandID)?.worth ?: 0.0
+            ).toString()
             else -> null
         }
     }
