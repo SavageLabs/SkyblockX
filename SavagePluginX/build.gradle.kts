d5ec52adaf712d51745e26a622d2d0bab06e016e
Naman Gupta <naman0311@gmail.com>
2020-06-27 22:20:26 -0500

    Build file.

---
+++ SavagePluginX/build.gradle.kts
@@ -0,0 +1,47 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
+}
+
+group = "net.savagelabs"
+version = "v1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+    maven("https://nexus.savagelabs.net/repository/maven-releases/")
+}
+
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.1")
+
+
+
+    compileOnly("org.spigotmc:spigot-api:1.16.1-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    val shadowJar = named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        exclude("META-INF/*.DSA")
+        exclude("META-INF/*.RSA")
+
+        relocate("kotlin", "net.savagelabs.savagepluginx.shade.kotlin")
+
+
+        archiveBaseName.set("SavagePluginX")
+        minimize()
+    }
+}
