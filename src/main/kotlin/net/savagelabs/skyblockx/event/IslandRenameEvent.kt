2f50310cfd972fd66630df2ca4f6516841353f10
Naman Gupta <naman0311@gmail.com>
2020-07-01 10:48:32 -0500

    Island rename event.

---
+++ src/main/kotlin/net/savagelabs/skyblockx/event/IslandRenameEvent.kt
@@ -0,0 +1,20 @@
+package net.savagelabs.skyblockx.event
+
+import net.savagelabs.skyblockx.core.Island
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class IslandRenameEvent(val island: Island, var newName: String, val oldName: String) : Event(false) {
+
+    private val HANDLERS = HandlerList()
+    private var isCancelled = false
+
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
+
+    fun getHandlerList(): HandlerList? {
+        return HANDLERS
+    }
+
+}
