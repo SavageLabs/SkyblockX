69e0b6c326f03a76226f40f3894ee5487d29c07d
CAMOU <mgmt.camou@gmail.com>
2020-08-04 18:32:11 +0200

    Fix: Added 1.8 compatibility for custom skull-textures

--- src/main/kotlin/net/savagelabs/skyblockx/persist/data/SerializableItem.kt
+++ src/main/kotlin/net/savagelabs/skyblockx/persist/data/SerializableItem.kt
@@ -3,7 +3,9 @@ package net.savagelabs.skyblockx.persist.data
 import com.cryptomorin.xseries.XMaterial
 import com.deanveloper.skullcreator.SkullCreator
 import net.savagelabs.savagepluginx.item.ItemBuilder
+import org.bukkit.Bukkit
 import org.bukkit.inventory.ItemStack
+import java.util.*
 
 class SerializableItem(
 	var material: XMaterial,
@@ -14,12 +16,27 @@ class SerializableItem(
 ) {
 
 	fun buildItem(): ItemStack {
-		val builder = if (skullTexture != null) {
-			ItemBuilder(SkullCreator.itemFromBase64(skullTexture))
-		} else {
-			ItemBuilder(material.parseItem()!!)
-		}
-		return builder.name(name).lore(lore).amount(amt).glowing(false).build()
-	}
+		if (Bukkit.getVersion().contains("1.8")) {
+			//> 1.8 SkullCreation
+			val builder = if (skullTexture != null) {
+				val bytes: ByteArray = Base64.getUrlDecoder().decode(skullTexture)
+				val textureUrl: String = String(bytes).substring(28).dropLast(4)
 
+				ItemBuilder(SkullCreator.itemFromUrl(textureUrl))
+			} else {
+				ItemBuilder(material.parseItem()!!)
+			}
+
+			return builder.name(name).lore(lore).amount(amt).glowing(false).build()
+		} else {
+			//> 1.9+ SkullCreation
+			val builder = if (skullTexture != null) {
+				ItemBuilder(SkullCreator.itemFromBase64(skullTexture))
+			} else {
+				ItemBuilder(material.parseItem()!!)
+			}
+
+			return builder.name(name).lore(lore).amount(amt).glowing(false).build()
+		}
+	}
 }
