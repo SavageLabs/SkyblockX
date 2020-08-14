0a7645c20069bbc01e656920f1b093e310f490f9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-14 18:29:28 +0200

    Feature: Added 1.16.2 Support

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     compileOnly(kotlin("stdlib-jdk8"))
 
-    compileOnly("org.spigotmc:spigot-api:1.16.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.16.2-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.9.2")
 
     // Present in XCore -- for IntelliJ indexing.
